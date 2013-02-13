651380401e9f0ec9bfe047b56a6ec1d1d2c1f3dd
decoverri <decoverri@gmail.com>
2013-02-13 15:13:55 -0200

    Adding Treasure H and I to new json

--- src/com/decoverri/treasureGenerator/simulators/GeneratorTest.java
+++ src/com/decoverri/treasureGenerator/simulators/GeneratorTest.java
@@ -11,8 +11,8 @@ public class GeneratorTest {
 	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
 		long inicio = System.currentTimeMillis();
 
-		int worth = 10000;
-		char type = 'G';
+		int worth = 100000;
+		char type = 'I';
 
 		TreasureGenerator generator = new TreasureGenerator();
 		List<Treasure> treasures = generator.genarate(worth, type);
