e0e3cdcf1fefc71d08fbd2ccfcf9c91c6f082d11
Deco Verri <decoverri@gmail.com>
2013-12-10 18:44:24 -0200

    Fit data shiffing from a main method to a test one. Outside my context

--- src/test/java/com/decoverri/treasureGenerator/fitData/FitData.java
+++ src/test/java/com/decoverri/treasureGenerator/fitData/FitData.java
@@ -1,27 +1,58 @@
 package com.decoverri.treasureGenerator.fitData;
 
+import java.io.FileInputStream;
+import java.util.Scanner;
+
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.decoverri.treasureGenerator.config.HibernateUtil;
+import com.decoverri.treasureGenerator.dao.TreasureTypeDao;
+import com.decoverri.treasureGenerator.dao.TreasureTypeValueDao;
+import com.decoverri.treasureGenerator.model.TreasureType;
+import com.decoverri.treasureGenerator.model.TreasureTypeValue;
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.json.JettisonMappedXmlDriver;
 
 public class FitData {
 
 	private static final Session session = HibernateUtil.getSessionFactory().getCurrentSession();
 
-	@Test
-	public void fitTreasureTypes() throws Exception {
-		Transaction transaction = session.beginTransaction();
-		new FitTreasureTypes(session).fit();
+	private Transaction transaction;
+	
+	private XStream xstream = new XStream(new JettisonMappedXmlDriver());
+
+	@Before
+	public void before() {
+		transaction = session.beginTransaction();
+	}
+
+	@After
+	public void after() {
 		transaction.commit();
 	}
 	
-//	public static void main(String[] args) throws IOException {
-//		long inicio = System.currentTimeMillis();
-//
-//		
-//
+	@Test
+	public void fitTreasureTypes() throws Exception {
+		TreasureTypeDao treasureTypesDao = new TreasureTypeDao(session);
+		TreasureTypeValueDao treasureTypeValueDao = new TreasureTypeValueDao(session);
+
+		xstream.alias("type", TreasureType.class);
+		xstream.alias("value", TreasureTypeValue.class);
+
+		Scanner scanner = new Scanner(new FileInputStream("dataInTxt/treasureTypes.json"));
+		while (scanner.hasNextLine()) {
+			TreasureType type = (TreasureType) xstream.fromXML(scanner.nextLine());
+			treasureTypeValueDao.saveOrLoad(type.getValues());
+			treasureTypesDao.saveOrUpdate(type);
+		}
+		scanner.close();
+
+	}
+	
 //		new FitGems(session).fit();
 //
 //		new FitArtObjects(session).fit();
@@ -58,8 +89,4 @@ public class FitData {
 //		
 //		new FitTreasure(session).fit();
 //
-//
-//		long fim = System.currentTimeMillis();
-//		System.out.println("Total time: " + (fim - inicio)/1000.0 + "s");
-//	}
 }
